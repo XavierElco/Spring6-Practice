add6f8e606f62e61890e28230370d50085a0ad4d
XavierElco <ccaihong@icloud.com>
2025-12-15 19:03:08 +0800

    静态代理和jdk动态代理练习

--- SpringProxy01/src/main/java/com/xav/proxydemo03/Host.java
+++ SpringProxy01/src/main/java/com/xav/proxydemo03/Host.java
@@ -1,4 +1,8 @@
 package com.xav.proxydemo03;
 
 public class Host {
+    public Object rent(double money) {
+        System.out.println("房东要出租房子，租金：" + money);
+        return new Object();
+    }
 }
